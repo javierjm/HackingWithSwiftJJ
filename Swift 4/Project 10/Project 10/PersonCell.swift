8936b5cbe977f78d20ca9e25af5e454d80c497a4
Javier Jara <javier.jara@logn.co>
2017-10-25 08:26:12 -0600

    Add Project 10

---
+++ Swift 4/Project 10/Project 10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Javier Jara on 10/11/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
